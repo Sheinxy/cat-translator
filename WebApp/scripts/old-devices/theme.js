c347afc1d3d6beafbe385095d360441d22b0d596
Raphaël <raphitou.montes@gmail.com>
2019-07-31 02:01:35 +0200

    v2.1.1 - Better Android support + new logo
    -Added support for Android <7.0
    -Added a new logo by Takase
    -Added a better style for mobile users

---
+++ WebApp/scripts/old-devices/theme.js
@@ -0,0 +1,19 @@
+var theme = document.querySelector("#theme");
+var button = document.querySelector("#themeButton");
+var icon = document.querySelector("#icon");
+
+var current = getCookie('theme') ? getCookie('theme') : 'light';
+theme.href = './styles/' + current + '.css';
+
+function changeTheme() {
+    if (current === "dark") {
+        current = 'light';
+        button.innerHTML = 'Dark Theme';
+    } else {
+        current = 'dark';
+        button.innerHTML = 'Light Theme';
+    }
+    setCookie('theme', current, 365)
+    theme.href = './styles/' + current + '.css';
+    icon.href = './assets/icon-' + current + '.png';
+}
